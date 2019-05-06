45dbef56ebf6dca8171561aaf354eb523e06f768
Vladislav Georgiev <vlad.georgiev@outlook.com>
2019-05-06 13:58:43 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,5 +1,9 @@
-def my_collect(collection)
+def my_collect(array)
   i = 0
-
-  while i <
+  collect = []
+  while i < array.length
+    collect << yield(array[i])
+    i += 1
+  end
+  collect
 end
